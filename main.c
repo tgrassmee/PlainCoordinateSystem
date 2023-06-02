ba6b4e314e5201e9dd0acc1a2a88506219b00291
Thomas Grassmee <tomboy0815@web.de>
2023-06-02 16:23:35 +0200

    Version 1.0

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 int main() {
 
-    Point points[]={ {10,10}};
+    Point points[]={ {1,1},{2,2},{3,3},{4,4},{5,5},{6,6},{7,7},{8,8},{9,9},{10,10},{11,11},{12,12},{13,13},{14,14},{15,15},{16,16},{17,17},{18,18}};
 
     int size = sizeof(points)/sizeof(points[0]);
     draw_coordinatesystem(points, size);
--- src/coordinatesystem.c
+++ src/coordinatesystem.c
@@ -1,4 +1,30 @@
 //
+// Created by tombo on 02.06.2023.
+//
+#include "../include/coordinatesystem.h"
+#include <stdio.h>
+
+
+#define X_MAX 20
+#define Y_MAX 20
+#define FORMATFAKTOR 3
+
+
+void draw_coordinatesystem(Point points[], int size) {
+    // Bildschirm löschen und Cursor ausblenden
+    printf("\x1b[2J\x1b[?25l");
+
+    // Zeichnen der Y-Achse mit Beschriftung und Pfeil
+    printf("   y\n");
+    printf("   ^\n");
+    for (int y = Y_MAX; y >= 0; y--) {
+        printf("%2d |", y);
+        for (int x = 1; x < X_MAX*FORMATFAKTOR; x++) {
+            int markiert = 0;
+            for (int i = 0; i < size; i++) {
+                if (points[i].x == x && points[i].y == y) {
+                    printf(" O ");  // Markierung des Punkts
+                    markiert = 1;
                     break;
                 }
             }
